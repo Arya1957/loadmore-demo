953027594203475ded9d30e3349201b4c83df8a9
duanlei <1969563716@qq.com>
2018-03-24 10:28:42 +0800

    update

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-/* 约定 ：
-1. method: GET
-2. 接口名称： /loadmore
-3.入参 ：{
-    index: 3,
-        length :5
-}
-4.后台响应数据：['内容3','内容4']
-*/
-
 
 // 服务器;
+var http = require('http');
+var path = require('path');
+var fs = require('fs'); //读写文件
+var url = require('url');
 
+var server = http.createServer(function(req,res){
 
+    var pathObj = url.parse(req.url, true);
+    console.log(pathObj);
 
-app.get('/loadmore',function(req,res){
+    switch (pathObj.pathname) {
+        case '/loadmore':
 
-    var curIdx = req.query.index;
-    var len = req.query.length;
-    var data = [];
+            var curIndex = pathObj.query.index;
+            var len = pathObj.query.length;
+            var data = [];
 
-    for(var i = 0;i < len;i++){
-        data.push('内容'+(parseInt(curIdx)+i))
+            for(var i =0 ;i<len;i++){
+                data.push('内容'+(parseInt(curIndex)+i))
+            }
+            res.end(JSON.stringify(data));
+
+            break;
+
+        default:
+            fs.readFile(path.join(__dirname,'static',pathObj.pathname),function(err,data){
+                if(err){
+                    res.statusCode = 404;
+                    res.end('Not Found')
+                } else {
+                    res.end(data)
+                }
+            })
     }
-    res.send(data);
 });
+
+server.listen(8080);
+
+
--- static/index.js
+++ static/index.js
@@ -1,67 +1,74 @@
+/*
+1.约定接口类型为GET，接口名称为 /loadmore
+2. 约定入参的 数据类型为JSON
+*/
+
 var btn = document.querySelector('#load-more'),
     ct = document.querySelector('#ct'),
-    curIndex = 2;
+    curIndex = 3, //当前要加载的数据的序号
+    length = 6; //每次要加载的数据个数
 
-var isDataArrive = true; // 设置状态锁
+var loading = false; // 状态锁，用于判断是否在加载数据
+
 btn.addEventListener('click', function (e) {
     e.preventDefault(); //阻止点击a时跳到页面顶部，也可以设置 a href = 'javascript:void(0)'
 
-    if (!isDataArrive) {
-        return;
+    if (loading) {
+        return;  // 如果正在加载数据，就什么都不做
     }
-    loadData(function (lists) {
-        renderPage(lists);
-        curIndex += 6;
-        isDataArrive =true
-    });
-    isDataArrive = false
+    loading = true; // 发请求之前先做个标记，上锁
+    ajax('/loadmore', {
+        index: curIndex,
+        length: length
+    }, function (data) {
+        appendData(data);
+        loading = false; //数据到来之后解锁
+        curIndex = curIndex + length; //修改序号
+        console.log('curIndex:' + curIndex);
+    })
 });
 
 
-function loadData(callback) {
-    ajax({
-        type: 'get',
-        url: '/loadmore',
-        data: {
-            index: curIndex,
-            length: 5
-        },
-        onSuccess: callback, // 成功的时候调用的函数
-        onError: function () {
-            console.log('error')
-        } // 失败的时候调用的函数
-    })
-}
-
-function renderPage(lists) {
+function appendData(data) {
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < lists.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         var nodes = document.createElement('li');
-        nodes.innerText = lists[i];
-        fragment.appendChild(nodes)
+        nodes.innerText = data[i];
+
+        console.log('data[i]:' + data[i]); // 内容i
+
+        fragment.appendChild(nodes);
     }
     ct.appendChild(fragment);
+
+
 }
 
 // 封装ajax
 
-function ajax(opts) {
+function ajax(url, json, onSuccess, onError) {
     var xhr = new XMLHttpRequest();
+
+    var queryStr = '?';
+    for (key in json) {
+        queryStr += key + '=' + json[key] + '&';
+    }
+    queryStr = queryStr.substr(0, queryStr.length - 1);
+    url = url + queryStr; //拼接url
+
+    xhr.open('GET', url, true);
+    xhr.send();
+
+
     xhr.onload = function () {
         if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
-            var data = JSON.parse(xhr.responseText);
-            opts.onSuccess(data)
+            console.log('this:' + this.response);
+
+            onSuccess(JSON.parse(xhr.responseText));
+            console.log('responseText' + JSON.parse(xhr.responseText))
+
         } else {
-            opts.onError()
+            onError()
         }
-    };
-
-    var query = '?';
-    for(key in opts.data){
-        query += key + '=' + opts.data[key] + '&';
     }
-    query = query.substr(0,query.length-1);
-    xhr.open(opts.type, opts.url + query, true);
-    xhr.send()
-
 }
